5fe08b63f09eb201c440f2c7be4c494b372df0ed
Arrastia <arrastia@pm.me>
2022-03-19 23:04:52 +0100

    UPDATE - adapt styled-components DefaultTheme types

---
+++ src/ui/_styles/@types/Styles.types.ts
@@ -0,0 +1,16 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+      black: string;
+      darkGrey: string;
+      error: string;
+      lightGrey: string;
+      primary: string;
+      secondary: string;
+      transparent: string;
+      white: string;
+    };
+  }
+}
---
+++ src/ui/_styles/theme/index.ts
@@ -0,0 +1,12 @@
+export const theme = {
+  colors: {
+    black: '#000000',
+    darkGrey: '#A3B7B5',
+    error: '#FF0000',
+    lightGrey: '#DDEAEA',
+    primary: '#18C6AC',
+    secondary: '#F5F5F5',
+    transparent: 'transparent',
+    white: '#FFFFFF'
+  }
+};
