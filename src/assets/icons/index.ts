c29e7cb2f8b61e617afec563c140e825ad237f97
Arrastia <arrastia@pm.me>
2022-03-20 20:26:02 +0100

    ADD - Ambientes & Rutinas icons

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,2 +1,4 @@
+export { AmbientesIcon } from './Ambientes.icon';
 export { RinconesIcon } from './Rincones.icon';
+export { RutinasIcon } from './Rutinas.icon';
 export { TalleresIcon } from './Talleres.icon';
